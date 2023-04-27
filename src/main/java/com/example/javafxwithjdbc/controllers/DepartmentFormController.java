e74fefcbfc36858855492d7d590f551271a557fd
Vini Costa <viniciuscosta@ciandt.com>
2023-04-27 00:47:14 -0300

    Saving a new Department

--- src/main/java/com/example/javafxwithjdbc/controllers/DepartmentFormController.java
+++ src/main/java/com/example/javafxwithjdbc/controllers/DepartmentFormController.java
@@ -1,9 +1,14 @@
 package com.example.javafxwithjdbc.controllers;
 
 import com.example.javafxwithjdbc.model.entities.Department;
+import com.example.javafxwithjdbc.model.services.DepartmentService;
+import com.example.javafxwithjdbc.utils.Alerts;
 import com.example.javafxwithjdbc.utils.Constraints;
+import com.example.javafxwithjdbc.utils.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -14,6 +19,8 @@ import java.util.ResourceBundle;
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -31,17 +38,43 @@ public class DepartmentFormController implements Initializable {
     private Button btnCancel;
 
     @FXML
-    public void onBtnSaveAction() {
-        System.out.println("onBtnSaveAction");
+    public void onBtnSaveAction(ActionEvent event) {
+        if (entity == null) {
+            throw new IllegalStateException("Entity was null");
+        }
+        if (service == null) {
+            throw new IllegalStateException("Service was null");
+        }
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+        } catch (db.DbException e) {
+            Alerts.ShowAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setName(txtName.getText());
+
+        return obj;
     }
 
     @FXML
-    public void onBtnCancelAction() {
-        System.out.println("onBtnCancelAction");
+    public void onBtnCancelAction(ActionEvent event) {
+        Utils.currentStage(event).close();
     }
 
     public void setDepartment(Department entity) {
         this.entity = entity;
+    }
+
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
     }
 
     @Override
